e6f0ce474a9dd6aff9e0291a5cbd117f7777e2e3
khubaib-shahid <khubaibkingofk@gmail.com>
2025-02-10 18:52:22 +0500

    add version

--- app/sanityClient.ts
+++ app/sanityClient.ts
@@ -5,6 +5,7 @@ const client = createClient(
     {
     projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : "",
     dataset : process.env.NEXT_PUBLIC_SANITY_DATASET ? process.env.NEXT_PUBLIC_SANITY_DATASET : "",
+    apiVersion : "2024-12-14",
     useCdn : false,
     token : process.env.NEXT_PUBLIC_SANITY_TOKEN ? process.env.NEXT_PUBLIC_SANITY_TOKEN : ""
 }
