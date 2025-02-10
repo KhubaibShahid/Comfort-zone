94870db3fa94e3b75d2364a0b991af1e0cbb0346
khubaib-shahid <khubaibkingofk@gmail.com>
2025-02-10 18:58:05 +0500

    add conditional statement

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,35 +5,37 @@ import bcrypt from "bcrypt";
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const user = await client.fetch(
-      `*[_type == "user" && email == "${body.email}"]`
-    );
-
-    if (user.length) {
-      const compare = await bcrypt.compare(body.password, user[0].password).then(function(result) {
-        console.log(result, "result");
-        return result
-      });
-      if (compare) {
-        return NextResponse.json({
-          statusbar: 200,
-          user: user,
-          message: "login successfully",
+    if (client) {
+      const user = await client.fetch(
+        `*[_type == "user" && email == "${body.email}"]`
+      );
+      if (user.length) {
+        const compare = await bcrypt.compare(body.password, user[0].password).then(function(result) {
+          console.log(result, "result");
+          return result
         });
+        if (compare) {
+          return NextResponse.json({
+            statusbar: 200,
+            user: user,
+            message: "login successfully",
+          });
+        } else {
+          return NextResponse.json({
+            statusbar: 401,
+            type: "password",
+            message: "incorrect password",
+          });
+        }
       } else {
         return NextResponse.json({
           statusbar: 401,
-          type: "password",
-          message: "incorrect password",
+          type: "email",
+          message: "invalid email",
         });
       }
-    } else {
-      return NextResponse.json({
-        statusbar: 401,
-        type: "email",
-        message: "invalid email",
-      });
     }
+
   } catch (error) {
     console.log(error);
     return NextResponse.json({ statusbar: 401, message: error });
