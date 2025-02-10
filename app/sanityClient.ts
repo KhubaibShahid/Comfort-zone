1b035095d867a80e47a3aad720e43822ec632f9a
khubaib-shahid <khubaibkingofk@gmail.com>
2025-02-10 19:09:50 +0500

    ahnge environment

--- app/sanityClient.ts
+++ app/sanityClient.ts
@@ -3,7 +3,7 @@ import {createClient} from "@sanity/client"
 
 const client = createClient(
     {
-    projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : "",
+    projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : '',
     dataset : process.env.NEXT_PUBLIC_SANITY_DATASET ? process.env.NEXT_PUBLIC_SANITY_DATASET : "",
     apiVersion : "2024-12-14",
     useCdn : false,
